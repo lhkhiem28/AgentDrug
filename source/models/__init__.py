6c3dfc4f941c63fa612381a88d732aa8786502d8
lhkhiem28 <lhkhiem28@gmail.com>
2025-07-01 14:12:25 +0100

    .

--- source/models/__init__.py
+++ source/models/__init__.py
@@ -8,18 +8,14 @@ load_model = {
 get_llm_path = {
     'qwen2.5-3b'      : 'Qwen/Qwen2.5-3B-Instruct'                  ,
     'qwen2.5-7b'      : 'Qwen/Qwen2.5-7B-Instruct'                  ,
-    'qwen2.5-14b'     : 'Qwen/Qwen2.5-14B-Instruct'                 ,
-    'qwen2.5-32b'     : 'Qwen/Qwen2.5-32B-Instruct'                 ,
-    'r1-qwen2.5-14b'  : 'deepseek-ai/DeepSeek-R1-Distill-Qwen-14B'  ,
-    'r1-qwen2.5-32b'  : 'deepseek-ai/DeepSeek-R1-Distill-Qwen-32B'  ,
 
     'llama-3.1-8b'    : 'meta-llama/Llama-3.1-8B-Instruct'          ,
     'llama-3.1-70b'   : 'meta-llama/Llama-3.1-70B-Instruct'         ,
-    'r1-llama-3.1-8b' : 'deepseek-ai/DeepSeek-R1-Distill-Llama-8B'  ,
-    'r1-llama-3.1-70b': 'deepseek-ai/DeepSeek-R1-Distill-Llama-70B' ,
 
     'olmo-2-7b-sft'   : 'allenai/OLMo-2-1124-7B-SFT'                ,
-    'olmo-2-13b-sft'  : 'allenai/OLMo-2-1124-13B-SFT'               ,
     'olmo-2-7b-dpo'   : 'allenai/OLMo-2-1124-7B-DPO'                ,
+    'olmo-2-7b'       : 'allenai/OLMo-2-1124-7B-Instruct'           ,
+    'olmo-2-13b-sft'  : 'allenai/OLMo-2-1124-13B-SFT'               ,
     'olmo-2-13b-dpo'  : 'allenai/OLMo-2-1124-13B-DPO'               ,
+    'olmo-2-13b'      : 'allenai/OLMo-2-1124-13B-Instruct'          ,
 }
